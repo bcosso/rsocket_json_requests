75470bce4432d238a7bdf6f8ae37b7db609df6c6
bcosso <bcosso@gmail.com>
2022-11-18 17:53:03 +0100

    Update requests_manager.go

--- requests_manager.go
+++ requests_manager.go
@@ -5,11 +5,10 @@ import (
 	"fmt"
 	"log"
 	"encoding/json"
-
+	"crypto/tls"
 	"github.com/rsocket/rsocket-go"
 	"github.com/rsocket/rsocket-go/payload"
 	"github.com/rsocket/rsocket-go/rx/mono"
-
 )
 
 
@@ -20,6 +19,8 @@ func AppendFunctionHandler(name string, function func(pload interface{}) interfa
 	func_list = append(func_list, func1)
 }
 
+var tc * tls.Config
+
 type FunctionName struct{
 	Function func(pload interface{}) interface{};
 	Name string 
@@ -27,7 +28,9 @@ type FunctionName struct{
 
 var func_list []FunctionName
 
+
 func ServeCalls(){
+
 	err := rsocket.Receive().
 	Acceptor(func(ctx context.Context, setup payload.SetupPayload, sendingSocket rsocket.CloseableRSocket) (rsocket.RSocket, error) {
 		// bind responder
@@ -66,11 +69,44 @@ func ServeCalls(){
 
 				
 				meta_data, err := json.Marshal(resp)
-				return mono.Just(payload.New(data, meta_data))
+				if err!= nil{
+					fmt.Println(err)
+					log.Fatal(err)
+					fmt.Println(err)
+				}
+
+				return mono.Just(payload.New(meta_data, data))
 			}),
 		), nil
 	}).
-	Transport(rsocket.TCPServer().SetAddr(":7878").Build()).
+	Transport((func() *rsocket.TCPServerBuilder {
+		var builder_result *rsocket.TCPServerBuilder
+		builder_result = rsocket.TCPServer()
+			if tc != nil{
+				builder_result = builder_result.SetTLSConfig(tc)	
+			}
+			return builder_result
+		}()).SetAddr(":7878").Build()).
 	Serve(context.Background())
 	log.Fatalln(err)
+}
+
+func SetTLSConfig(cert_path string, key_path string){
+	cert, err := tls.LoadX509KeyPair(cert_path, key_path)
+	if err != nil {
+		panic(err)
+	}
+	// Init TLS configuration.
+	tc = &tls.Config{
+		MinVersion:               tls.VersionTLS12,
+		CurvePreferences:         []tls.CurveID{tls.CurveP521, tls.CurveP384, tls.CurveP256},
+		PreferServerCipherSuites: true,
+		CipherSuites: []uint16{
+			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
+			tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_RSA_WITH_AES_256_CBC_SHA,
+		},
+		Certificates: []tls.Certificate{cert},
+	}
 }
