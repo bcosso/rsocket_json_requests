efda7ad06c9fdc9d5b09d8da88702feedab52637
bcosso <bcosso@gmail.com>
2025-09-22 14:26:36 +0200

    Update requests_client.go
    Adding long living connections

--- requests_client.go
+++ requests_client.go
@@ -2,17 +2,24 @@ package rsocket_json_requests
 
 import (
 	"context"
-	"log"
-	"fmt"
 	"crypto/tls"
+	"fmt"
+	"log"
+
+	jsoniter "github.com/json-iterator/go"
+
 	"github.com/rsocket/rsocket-go"
 	"github.com/rsocket/rsocket-go/payload"
 )
 
-type GenericList struct{
-	Method string `json:"method"`
+type GenericList struct {
+	Method  string      `json:"method"`
 	Payload interface{} `json:"payload"`
 }
+
+var jsonIterGlobal = jsoniter.Config{
+	EscapeHTML: false,
+}.Froze()
 
 var _ip string
 var _port int
@@ -20,58 +27,90 @@ var _port int
 var _genericList GenericList
 var _use_TLS bool
 
-
-func RequestConfigs(ip string, port int){
+func RequestConfigs(ip string, port int) {
 	_ip = ip
 	_port = port
 }
 
-func UseTLS(){
-	_use_TLS = true;
+func UseTLS() {
+	_use_TLS = true
 }
 
+type ClientRsock struct {
+	Cli rsocket.Client
+}
 
-func RequestJSON(method string, json_content interface {}) (interface {}, error) {
-	// Connect to server
-	var result_json interface{}
+var _cli map[string]ClientRsock
+
+// Starts singleton connection with server
+//
+// Parameters:
+//   - name: unique name of the instance
+//   - host: the host or ip address
+//   - port: the port of the host
+func InitConn(name string, host string, port int) error {
+	if _cli == nil {
+		_cli = make(map[string]ClientRsock)
+	}
 	_tc := &tls.Config{
 		InsecureSkipVerify: true,
 	}
 	cli, err := rsocket.Connect().
 		SetupPayload(payload.NewString("", "")).
-		Transport((func() *rsocket.TCPClientBuilder{
-			
-				var builder_result *rsocket.TCPClientBuilder
-				builder_result = rsocket.TCPClient()
-				if _use_TLS == true{
-					builder_result = builder_result.SetTLSConfig(_tc)	
-				}
-				return builder_result
-				
-		}()).SetHostAndPort(_ip, _port).Build()).
+		Transport((func() *rsocket.TCPClientBuilder {
+
+			var builder_result *rsocket.TCPClientBuilder
+			builder_result = rsocket.TCPClient()
+			if _use_TLS == true {
+				builder_result = builder_result.SetTLSConfig(_tc)
+			}
+			return builder_result
+
+		}()).SetHostAndPort(host, port).Build()).
 		Start(context.Background())
 	if err != nil {
 		//panic(err)
-		return nil, err
+		return err
 	}
-	defer cli.Close()
+	var cliStruct ClientRsock
+	cliStruct.Cli = cli
+	_cli[name] = cliStruct
+
+	return nil
+
+}
+
+func CloseConn(name string) {
+	_cli[name].Cli.Close()
+}
+
+// Creates a request with an existing connection to an instance.
+//
+// Parameters:
+//   - method: name of the method to be called in the endpoint
+//   - json_content: parameters to be sent to the endpoint in JSON
+//   - name: unique name of the instance
+func RequestJSONNew(method string, json_content interface{}, name string) (interface{}, error) {
+	// Connect to server
+	var result_json interface{}
+
+	// defer cli.Close()
 	_genericList.Method = method
-	method = "{\"method\":\""+ method +"\"}"
+	method = "{\"method\":\"" + method + "\"}"
 	data := []byte(method)
 
-	
 	_genericList.Payload = json_content
-	
+
 	meta_data, err := jsonIterGlobal.Marshal(_genericList)
-	result, err := cli.RequestResponse(payload.New(meta_data, data)).Block(context.Background())
+	result, err := _cli[name].Cli.RequestResponse(payload.New(meta_data, data)).Block(context.Background())
 	if err != nil {
 		//panic(err)
 		return nil, err
 	}
-	
+
 	err = jsonIterGlobal.Unmarshal(result.Data(), &result_json)
 
-	if err!= nil{
+	if err != nil {
 		fmt.Println(err)
 		log.Fatal(err)
 		fmt.Println(err)
@@ -80,13 +119,9 @@ func RequestJSON(method string, json_content interface {}) (interface {}, error)
 	return result_json, nil
 }
 
-type ClientRsock struct {
-	Cli rsocket.Client
-}
-
-var _cli ClientRsock
-
-func InitConn() error {
+func RequestJSON(method string, json_content interface{}) (interface{}, error) {
+	// Connect to server
+	var result_json interface{}
 	_tc := &tls.Config{
 		InsecureSkipVerify: true,
 	}
@@ -105,22 +140,9 @@ func InitConn() error {
 		Start(context.Background())
 	if err != nil {
 		//panic(err)
-		return err
+		return nil, err
 	}
-	_cli.Cli = cli
-
-	return nil
-}
-
-func CloseConn() {
-	_cli.Cli.Close()
-}
-
-func RequestJSONNew(method string, json_content interface{}) (interface{}, error) {
-	// Connect to server
-	var result_json interface{}
-
-	// defer cli.Close()
+	defer cli.Close()
 	_genericList.Method = method
 	method = "{\"method\":\"" + method + "\"}"
 	data := []byte(method)
@@ -128,7 +150,7 @@ func RequestJSONNew(method string, json_content interface{}) (interface{}, error
 	_genericList.Payload = json_content
 
 	meta_data, err := jsonIterGlobal.Marshal(_genericList)
-	result, err := _cli.Cli.RequestResponse(payload.New(meta_data, data)).Block(context.Background())
+	result, err := cli.RequestResponse(payload.New(meta_data, data)).Block(context.Background())
 	if err != nil {
 		//panic(err)
 		return nil, err
@@ -137,12 +159,14 @@ func RequestJSONNew(method string, json_content interface{}) (interface{}, error
 	err = jsonIterGlobal.Unmarshal(result.Data(), &result_json)
 
 	if err != nil {
+		fmt.Println(err)
 		log.Fatal(err)
 		fmt.Println(err)
 		return nil, err
 	}
 	return result_json, nil
 }
+
 
 
 
